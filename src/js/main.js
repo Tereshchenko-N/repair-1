cb022a1c93efa6cac6006999f38f683257156712
Natalia <xramono4ka@gmail.com>
2019-06-02 23:24:08 +0300

    Добавлен скрипт, который закрывает модальное окно через 5 секунд

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ var button = document.querySelector('#btn');
 var modal = document.querySelector('#modal');
 var close = document.querySelector('#close');
 
+
 button.addEventListener('click', function() {
   modal.classList.add('modal__active');
 });
@@ -9,3 +10,7 @@ button.addEventListener('click', function() {
 close.addEventListener('click', function() {
  modal.classList.remove('modal__active');
 });
+//Закрытие модального окна через 5 секунд 
+setTimeout(function () {
+  document.getElementById('modal').style.display = 'none';
+}, 5000);
